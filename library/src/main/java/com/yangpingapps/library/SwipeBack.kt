6471b94686672a7b962a7a0e65e74b5fd61e2872
Ocean Lin <ocean.lin@pay2go.com>
2019-01-23 17:54:20 +0800

    Fix the bug of Listener in SwipeBack.

--- library/src/main/java/com/yangpingapps/library/SwipeBack.kt
+++ library/src/main/java/com/yangpingapps/library/SwipeBack.kt
@@ -32,7 +32,7 @@ class SwipeBack(activity: Activity, direction: SwipeListener.Direction) {
         return this
     }
 
-    fun setLaunchAnimation(switch:Boolean):SwipeBack{
+    fun setLaunchAnimation(switch: Boolean): SwipeBack {
         mParams.isAnimation = switch
         return this
     }
@@ -42,28 +42,38 @@ class SwipeBack(activity: Activity, direction: SwipeListener.Direction) {
         mParams.activity.setContentView(rootView)
         if (mParams.isAnimation) {
             when (mParams.direction) {
-                SwipeListener.Direction.RIGHT -> mParams.activity.overridePendingTransition(R.anim.slide_in_left, android.R.anim.fade_out)
-                SwipeListener.Direction.LEFT -> mParams.activity.overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out)
-                SwipeListener.Direction.UP -> mParams.activity.overridePendingTransition(R.anim.slide_in_top, android.R.anim.fade_out)
-                SwipeListener.Direction.DOWN -> mParams.activity.overridePendingTransition(R.anim.slide_in_bottom, android.R.anim.fade_out)
+                SwipeListener.Direction.RIGHT -> mParams.activity.overridePendingTransition(
+                    R.anim.slide_in_left,
+                    android.R.anim.fade_out
+                )
+                SwipeListener.Direction.LEFT -> mParams.activity.overridePendingTransition(
+                    R.anim.slide_in_right,
+                    android.R.anim.fade_out
+                )
+                SwipeListener.Direction.UP -> mParams.activity.overridePendingTransition(
+                    R.anim.slide_in_top,
+                    android.R.anim.fade_out
+                )
+                SwipeListener.Direction.DOWN -> mParams.activity.overridePendingTransition(
+                    R.anim.slide_in_bottom,
+                    android.R.anim.fade_out
+                )
             }
         }
     }
+
     private fun getContentLayout(params: Params): View {
         if (params.layoutResID == -1) {
             throw NullPointerException("The ContentView is Null.")
         }
         val viewShadow = View(params.activity)
-        if (params.listener == null) {
-            params.listener = object : OnSwipeListener {
-                override fun onSwiped(persent: Float, position: Float) {
-                    viewShadow.alpha = 1 - persent
-                }
-            }
-        }
-
         val view = LayoutInflater.from(params.activity).inflate(params.layoutResID, null)
-        view.setOnTouchListener(SwipeListener(params.activity, params.listener!!, params.direction))
+        view.setOnTouchListener(SwipeListener(params.activity, object : OnSwipeListener {
+            override fun onSwiped(persent: Float, position: Float) {
+                viewShadow.alpha = 1 - persent
+                mParams.listener?.onSwiped(persent, position)
+            }
+        }, params.direction))
         val root = FrameLayout(params.activity)
         viewShadow.setBackgroundColor(params.shadow)
         val layoutParams = FrameLayout.LayoutParams(
